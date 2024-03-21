3e985514f59a8542d1776f937fadc6ba463f4359
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-03-21 12:00:53 -0300

    feat: displaying stack content similar to an array

--- Stacks/stack.js
+++ Stacks/stack.js
@@ -26,6 +26,33 @@ class Stack {
         delete this.items[this.count];
         return result;
     }
+
+    peek(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        return this.items[this.count - 1]; //é expressão usada para acessar o índice do último elemento na pilha. (2 - 1) = 1
+    }
+
+    clear(){
+        this.items = {};
+        this.count = 0;
+        // ou
+        while (!this.isEmpty()) {
+            this.pop();
+        }
+    }
+
+    //Exibindo o conteudo de pilha semelhante a um array
+    toString(){
+        if (this.isEmpty()) {
+            return '';
+        }
+        let objString = `${this.items[0]}`;
+        for (let i = 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[1]}`
+        }
+    }
 }
 
 const stack = new Stack();
