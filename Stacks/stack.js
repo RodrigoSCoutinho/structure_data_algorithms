9e04858cb68c67bd6fdcbcb32acd5ba70e979893
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2024-03-21 11:39:55 -0300

    feat: adding the method pop for remove the element

--- Stacks/stack.js
+++ Stacks/stack.js
@@ -8,6 +8,24 @@ class Stack {
         this.items[this.count] = element;
         this.count++;
     }
+
+    size(){
+        return this.count;
+    }
+
+    isEmpty(){
+        return this.count == 0;
+    }
+
+    pop(){
+        if(this.isEmpty()){
+            return undefined;
+        }
+        this.count--;
+        const result = this.items[this.count];
+        delete this.items[this.count];
+        return result;
+    }
 }
 
 const stack = new Stack();
@@ -15,3 +33,6 @@ const stack = new Stack();
 stack.push(5);
 stack.push(8);
 console.log(stack); // Stack { count: 2, items: { '0': 5, '1': 8 } }
+
+console.log(stack.pop())
+console.log(stack); // Stack { count: 1, items: { '0': 5 } }
